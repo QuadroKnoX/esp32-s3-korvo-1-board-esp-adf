dc1c8ce070f1397e213ca1994e05d4d2e7fd282e
Oliver Freudrich <QuadroKnoX@users.noreply.github.com>
2024-07-14 13:12:49 +0200

    * fixed missing method + fixed wrong type

--- esp32_s3_korvo1/board_pins_config.c
+++ esp32_s3_korvo1/board_pins_config.c
@@ -50,7 +50,7 @@ esp_err_t get_i2c_pins(i2c_port_t port, i2c_config_t *i2c_config)
     return ESP_OK;
 }
 
-esp_err_t get_i2s_pins(i2s_port_t port, board_i2s_pin_t *i2s_config)
+esp_err_t get_i2s_pins(i2s_port_t port, i2s_pin_config_t *i2s_config)
 {
     AUDIO_NULL_CHECK(TAG, i2s_config, return ESP_FAIL);
     if (port == I2S_NUM_0) { // Codec
@@ -66,7 +66,7 @@ esp_err_t get_i2s_pins(i2s_port_t port, board_i2s_pin_t *i2s_config)
         i2s_config->data_in_num = GPIO_NUM_11;
         i2s_config->mck_io_num = GPIO_NUM_20;
     } else {
-        memset(i2s_config, -1, sizeof(board_i2s_pin_t));
+        memset(i2s_config, -1, sizeof(i2s_pin_config_t));
         ESP_LOGE(TAG, "i2s port %d is not supported", port);
         return ESP_FAIL;
     }
@@ -88,6 +88,16 @@ esp_err_t get_spi_pins(spi_bus_config_t *spi_config, spi_device_interface_config
     spi_device_interface_config->spics_io_num = -1;
 
     ESP_LOGW(TAG, "SPI interface is not supported");
+    return ESP_OK;
+}
+
+esp_err_t i2s_mclk_gpio_select(i2s_port_t i2s_num, gpio_num_t gpio_num)
+{
+    if (i2s_num >= I2S_NUM_MAX) {
+        ESP_LOGE(TAG, "Does not support i2s number(%d)", i2s_num);
+        return ESP_ERR_INVALID_ARG;
+    }
+    ESP_LOGI(TAG, "I2S%d, MCLK output by GPIO%d", i2s_num, gpio_num);
     return ESP_OK;
 }
 
